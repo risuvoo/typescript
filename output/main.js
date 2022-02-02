af4a8c6409c17de3c841074ee6417921f47d6fe6
mexuvo <rafiqulislamsuvobd@gmail.com>
2022-02-02 11:15:49 +0600

    number type

--- output/main.js
+++ output/main.js
@@ -1,3 +1,8 @@
 "use strict";
 const info = "frontend developer";
 console.log(info);
+//type number
+function checkNumber(a, b) {
+    return a * b;
+}
+console.log(checkNumber(2, 2));
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,9 @@
 const info = "frontend developer";
 console.log(info);
+
+//type number
+function checkNumber(a :number, b:number ) {
+    return a * b;
+    
+}
+console.log(checkNumber(2,2))
